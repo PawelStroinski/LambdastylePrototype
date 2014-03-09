e09cc7e2a355a49f6f618e6e779d93404308023c
Paweł Stroiński <pol84@live.com>
2014-03-09 18:06:17 +0000

    new test: change id to file2

---
+++ Test/input2.json/2 change id to file2/Style.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input2.json._2_change_id_to_file2
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Id("id")), new Predicate(new OuterId(), new Raw("\"file2\""))),
+                    Consts.CopyAny);
+        }
+    }
+}
