477a8b528d7617956668e200d9d1cf04611732a9
Paweł Stroiński <pol84@live.com>
2014-04-05 12:35:44 +0100

    new working: similar to convert widget to an array

---
+++ Interpreter/Predicates/Cases/IdAndNoValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LambdastylePrototype.Interpreter.Predicates.Cases
+{
+    class IdAndNoValue : Case
+    {
+        PredicateContext context;
+        PredicateElement[] elements;
+
+        public override PredicateElement[] ApplyTo(CaseContext caseContext)
+        {
+            context = caseContext.Context;
+            elements = caseContext.Elements;
+            if (AppliesAt())
+                return elements.Where(element => !(element is OuterId)).ToArray();
+            else
+                return elements;
+        }
+
+        bool AppliesAt()
+        {
+            var hasIdAndDoesntHaveValue = elements.Any(element => element is OuterId)
+                && !elements.Any(element => element is ValueBase);
+            return hasIdAndDoesntHaveValue && context.ApplyingTail;
+        }
+    }
+}
---
+++ Test/input3.json/3 similar to convert widget to an array/Style.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input3.json._3_similar_to_convert_widget_to_an_array
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Id("widget")), new Predicate(new OuterId(), new Raw("[]"))),
+                Consts.CopyAny);
+        }
+    }
+}
