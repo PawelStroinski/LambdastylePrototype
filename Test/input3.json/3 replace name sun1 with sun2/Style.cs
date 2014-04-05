6f037e9352803d0bdfac2e9bb24f3af325c588eb
Paweł Stroiński <pol84@live.com>
2014-04-05 11:50:09 +0100

    new test: replace name "sun1" with "sun2"

---
+++ Test/input3.json/3 replace name sun1 with sun2/Style.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input3.json._3_replace_name_sun1_with_sun2
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Equals(new Id("name"), new Literal("sun1"))),
+                    new Predicate(new OuterId(), new Raw("\"sun2\""))),
+                Consts.CopyAny);
+        }
+    }
+}
