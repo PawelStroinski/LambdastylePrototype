394b83e6ebdf77eff6ec264acde8c769d5da711e
Paweł Stroiński <pol84@live.com>
2014-04-06 22:24:37 +0100

    new test: output copy only if debug=on

---
+++ Test/input3.json/3 output copy only if debug on 2/Style.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input3.json._3_output_copy_only_if_debug_on_2
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Equals(new Id("debug"), new Literal("on"))), new Predicate(),
+                    Consts.CopyAny));
+        }
+    }
+}
