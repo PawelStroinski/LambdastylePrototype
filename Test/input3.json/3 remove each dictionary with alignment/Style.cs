e9a3084b70bc4ded9350d1389281d51bf1228cf9
Paweł Stroiński <pol84@live.com>
2014-04-06 22:11:42 +0100

    new test: remove each dictionary with "alignment"

---
+++ Test/input3.json/3 remove each dictionary with alignment/Style.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input3.json._3_remove_each_dictionary_with_alignment
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Parent(new Id("alignment"))), new Predicate()),
+                Consts.CopyAny);
+        }
+    }
+}
