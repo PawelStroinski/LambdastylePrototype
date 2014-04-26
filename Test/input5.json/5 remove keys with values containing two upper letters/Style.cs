9bf6dadcf5b3db623209aef1dd6d42efde35fa59
Paweł Stroiński <pol84@live.com>
2014-04-26 13:03:18 +0200

    new test: remove keys with values containing two upper letters

---
+++ Test/input5.json/5 remove keys with values containing two upper letters/Style.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input5.json._5_remove_keys_with_values_containing_two_upper_letters
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Equals(new Any(), new RegExp("[A-Z].*[A-Z]"))), new Predicate()),
+                Consts.CopyAny);
+        }
+    }
+}
