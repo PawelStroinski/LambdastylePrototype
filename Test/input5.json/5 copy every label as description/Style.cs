40c89a2fd7e5d3c99b26e313a31cab1a8b3a8d0d
Paweł Stroiński <pol84@live.com>
2014-04-27 11:01:28 +0200

    new test: copy every label as description

---
+++ Test/input5.json/5 copy every label as description/Style.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LambdastylePrototype;
+using LambdastylePrototype.Interpreter;
+using LambdastylePrototype.Interpreter.Predicates;
+using LambdastylePrototype.Interpreter.Subjects;
+
+namespace Test.input5.json._5_copy_every_label_as_description
+{
+    class Style
+    {
+        void Build(Builder builder)
+        {
+            builder.Add(
+                new Sentence(new Subject(new Id("label")),
+                    new Predicate(new Raw("\"description\": "), new OuterValue())),
+                new Sentence(new Subject(new Id("label")), Consts.Copy),
+                Consts.CopyAny);
+        }
+    }
+}
